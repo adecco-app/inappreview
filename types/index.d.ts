700bb61016ac7d49d3f000e9012a12a318459697
adecco-app <105737129+adecco-app@users.noreply.github.com>
2022-08-05 16:18:20 +0300

    Add files via upload

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Launches in-app review dialog.
+ *
+ * @returns {Promise<void>} Callback when operation is completed
+ *
+ * @example
+ * cordova.plugins.AppReview.requestReview();
+ */
+export function requestReview(): Promise<void>;
